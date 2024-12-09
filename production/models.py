a10193d65ce1f2a22696af3c5035b19adbb630b0
Vitalii Safian <viisaf.prog.gt@gmail.com>
2024-12-09 23:30:42 +0200

    Create worker and workplace models in a production app

--- production/models.py
+++ production/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Workplace(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    phone_number = models.CharField(max_length=100, null=True, blank=True)
+    workplace = models.ForeignKey(
+        Workplace,
+        on_delete=models.SET_NULL,
+        null=True, blank=True,
+        related_name='workers',
+    )
+
+    def __str__(self):
+        return f"{self.username}"
--- wallis/urls.py
+++ wallis/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("production.urls", namespace="production")),
 
 ] + debug_toolbar_urls()
