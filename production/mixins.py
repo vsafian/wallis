01a7bbbe3a4eabe4b9a294f248a79ae7038bf2b0
Vitalii Safian <viisaf.prog.gt@gmail.com>
2025-01-16 21:04:56 +0200

    Refactor production/services.py; Remove associate_items_with_instance function

--- production/mixins.py
+++ production/mixins.py
@@ -2,12 +2,13 @@ from typing import Optional
 
 from django.core.exceptions import ImproperlyConfigured
 from django.db.models import QuerySet
-from django.forms import Field, ModelChoiceField, ModelMultipleChoiceField
+from django.forms import Field, BaseForm
 from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views import generic
 
+from production.services import filter_queryset_by_instance
 
 class DeleteViewMixin(generic.DeleteView):
     """
@@ -37,19 +38,26 @@ class ModelAbsoluteUrlMixin:
         view_name (str): The name of the URL pattern to reverse.
                          Must be defined in the subclass.
     """
-
     view_name: str = ""
+
+    def get_pk(self):
+        if hasattr(self, "pk"):
+            return self.pk
+        raise ImproperlyConfigured(
+            f"Model instance: <{self.__str__}> "
+            f"should be defined in the data base."
+        )
 
     def get_absolute_url(self) -> str:
         if not self.view_name:
             raise ImproperlyConfigured(
-                f"{self.__class__.__name__} "
+                f"{self.__name__} "
                 f"must define 'view_name' attribute."
             )
         return reverse(
             viewname=self.view_name,
             kwargs={
-                "pk": self.pk
+                "pk": self.get_pk(),
             }
         )
 
@@ -59,13 +67,14 @@ class ViewSuccessUrlMixin:
     ViewSuccessUrlMixin:
     - Provides a reusable implementation of the `get_success_url` method.
     """
-
     def get_success_url(self):
-        obj = self.get_object()
-        return (
-            obj.get_absolute_url()
+        if hasattr(self, "object"):
+            return (
+                self.object.get_absolute_url()
+            )
+        raise ImproperlyConfigured(
+            f"View should have an object attribute."
         )
-
 
 
 class InstanceCacheMixin:
@@ -90,14 +99,80 @@ class InstanceCacheMixin:
         return getattr(self, cache_name)
 
 
+class PostApproveMixin(generic.FormView):
+    def post(self, request, *args, **kwargs):
+        form = self.get_context_data()["form"]
+        if "approve" in request.POST and form.is_valid():
+            return self.form_valid(form)
+        return self.form_invalid(form)
+
+
 class FormFieldMixin:
     def get_field(self, field_name: str) -> Optional[Field]:
         """Return a form field by its name, if it exists."""
         if hasattr(self, "fields"):
             return self.fields.get(field_name, None)
 
-    def configure_widget_attrs(self, field_name: str, attrs: dict[str, str]) -> None:
+    @staticmethod
+    def field_queryset_exists(field: Optional[Field]) -> bool:
+        return field is True and hasattr(field, "queryset")
+
+    def configure_widget_attrs(
+            self,
+            field_name: str,
+            attrs: dict[str, str]
+    ) -> None:
         """Update widget attributes for the given field."""
         field = self.get_field(field_name)
         if field:
             field.widget.attrs.update(attrs)
+
+    def get_instance(self):
+        if hasattr(self, "instance"):
+            return self.instance
+
+    def get_initial_field(self, field_name: str) -> Field:
+        if hasattr(self, "initial"):
+            return self.initial.get(field_name, None)
+
+    def set_initial_default_queryset(
+            self,
+            field: str,
+        ) -> None:
+        instance = self.get_instance()
+        if hasattr(self, "initial"):
+            if instance and instance.pk:
+                self.initial.setdefault(
+                    field, getattr(instance, field).all()
+                )
+
+    def filter_field_queryset_by_instance(
+            self,
+            field_name: str,
+        ) -> None:
+        field = self.get_field(field_name)
+        if self.field_queryset_exists(field):
+            instance = self.get_instance()
+            field.queryset = filter_queryset_by_instance(
+                queryset=field.queryset, instance=instance,
+            )
+
+    @property
+    def initial_context(self):
+        """Ensures that the initiated form objects
+         are passed to the context
+         as objects, not as ids.
+        """
+        fields = {}
+        for field_name in self.fields:
+            initial_field_data = self.get_initial_field(field_name)
+            field = self.get_field(field_name)
+            if isinstance(initial_field_data, QuerySet):
+                fields[field_name] = self.get_initial_field(field_name)
+            else:
+                if hasattr(field, "queryset"):
+                    if isinstance(initial_field_data, int):
+                        fields[field_name] = field.queryset.get(
+                            **{"id": initial_field_data}
+                        )
+        return fields
--- production/services.py
+++ production/services.py
@@ -1,40 +1,57 @@
-import init_django_orm  # noqa F401
-
 from typing import Type
 from django.db import models
 from django.db.models import Q, QuerySet
 
 
-def get_model_plural(model: Type[models.Model] | models.Model) -> str:
-    return str(model._meta.verbose_name_plural).lower()
-
-
-def get_model_name(model: Type[models.Model] | models.Model) -> str:
-    return str(model._meta.model_name).lower()
-
-
-def foreign_case_help_text(
-        field_model: Type[models.Model] | models.Model,
-        foreign_model: Type[models.Model] | models.Model,
+def model_name_to_field(
+        model: Type[models.Model] | models.Model
 ) -> str:
-    return (
-        "Add a new or remove an existing "
-        f"{get_model_plural(field_model)} from the "
-        f"{get_model_name(foreign_model)}."
+    return "_".join(
+        model._meta.verbose_name.split()
     )
 
 
-def filter_queryset_by_instance(queryset: QuerySet, instance: models.Model):
+def model_to_related_name(model: Type[models.Model]) -> str:
+    return "_".join(
+        model._meta.verbose_name_plural.split()
+    )
+
+
+def foreign_case_help_text(
+        field_name: str,
+        instance_name: str,
+
+) -> str:
+    return (
+        "Add a new or remove an existing "
+        f"{field_name} from the "
+        f"{instance_name}."
+    )
+
+
+def filter_queryset_by_instance(
+        queryset: QuerySet,
+        instance: models.Model,
+):
     """
     Filters the queryset so that it contains objects,
-    that have a ForeignKey associated with instance or are None.
+    that have a ForeignKey associated with instance
+    or instance ForeignKey are None.
     """
-    instance_name = get_model_name(instance)
-    filter_kwargs = {
-        f"{instance_name}__isnull": True
+    instance_foreign_name = model_name_to_field(instance)
+    instance_none = {
+        f"{instance_foreign_name}__isnull": True
     }
-    filter_related = Q(**filter_kwargs) | Q(**{instance_name: instance})
-    return queryset.filter(filter_related).order_by(f"-{instance_name}")
+    if instance.pk:
+        filter_related = (
+                Q(**instance_none) |
+                Q(**{instance_foreign_name: instance})
+        )
+        return (
+            queryset.filter(filter_related)
+            .order_by(f"-{instance_foreign_name}")
+        )
+    return queryset.filter(**instance_none)
 
 
 def set_remove_foreign_by_cleaned_data_and_instance(
@@ -46,45 +63,36 @@ def set_remove_foreign_by_cleaned_data_and_instance(
     Update foreign key relationships for a target model based on cleaned form data.
 
     This function processes the many-to-one or many-to-many relationships of an instance
-    by checking which objects need to be added or removed. Objects that are already
-    related to the instance will be detached (set to None), while new objects will
-    be associated with the instance.
-
+    by checking which objects need to be added or removed.
+    If the previously connected objects
+    are not in the list of new objects, they will be disconnected.
     """
-    target_plural_name = get_model_plural(model_to_update)
-    instance_name = get_model_name(instance)
+    target_related_name = model_to_related_name(
+        model_to_update
+    )
+    instance_name = model_name_to_field(instance)
+
     new_objects = cleaned_data.get(
-        target_plural_name,
+        target_related_name,
         model_to_update.objects.none()
     )
+
     exists_objects = getattr(
-        instance, target_plural_name
+        instance, target_related_name
     ).all()
 
     to_update = []
 
-    for obj in new_objects:
-        if obj in exists_objects:
+    for obj in exists_objects:
+        if obj not in new_objects:
             setattr(obj, instance_name, None)
-        else:
+            to_update.append(obj)
+
+    for obj in new_objects:
+        if obj not in exists_objects:
             setattr(obj, instance_name, instance)
-        to_update.append(obj)
+            to_update.append(obj)
 
     model_to_update.objects.bulk_update(
         to_update, [instance_name]
     )
-
-def associate_items_with_instance(
-        instance: models.Model,
-        items: QuerySet[models.Model],
-        field_name:str = ""
-) -> None:
-    field = field_name if field_name else get_model_name(instance)
-    to_update = []
-
-    for obj in items:
-        setattr(obj, field, instance)
-        to_update.append(obj)
-
-    items.bulk_update(to_update, [field])
-
