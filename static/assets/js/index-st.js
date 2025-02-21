bd2fe9e71da7de98b9505e28f722e6f5aea81881
Vitalii Safian <viisaf.prog.gt@gmail.com>
2025-02-21 14:10:38 +0200

    Update weekOrdersChart for index.html

--- static/assets/js/index-st.js
+++ static/assets/js/index-st.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (document.getElementById("weekOrdersChart")) {
 
         let weekOrdersChart = new Chartist.Line('#weekOrdersChart', {
-            labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+            labels: orderStats.labels,
             series: orderStats.series
         }, {
             lineSmooth: Chartist.Interpolation.cardinal({
